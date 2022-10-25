142ddc5edfea4d0a3fde3bf9f109fba767417198
Ruslan Karabalin <94182636+ruska112@users.noreply.github.com>
2022-10-25 22:08:58 +0600

    Add files via upload

---
+++ src/test/java/org/example/my/StringServiceTest.java
@@ -0,0 +1,30 @@
+package org.example.my;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StringServiceTest {
+
+    @Test
+    public void getCountOfStringsStartsWith() {
+        String[] strArr = new String[]{"1", "2", "3"};
+        assertEquals(1, StringService.getCountOfStringsStartsWith(strArr, '1'));
+    }
+
+    @Test
+    public void getCountOfStringEquals() {
+        String[] strArr = new String[]{"1", "12", "123"};
+        assertEquals(0, StringService.getCountOfStringsEquals(strArr, "2"));
+    }
+
+    @Test
+    public void getCountOfStringsWithoutSubstring() {
+        try {
+            String[] atrArr = new String[]{"b", "aa", "aaa"};
+            assertEquals(5, StringService.getCountOfStringsWithoutSubstring(atrArr, "a"));
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Error in getCountOfStringWithoutSubstring: " + exception.getMessage());
+        }
+    }
+}
