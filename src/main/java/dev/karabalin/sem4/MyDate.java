5b7397417c7b9c176d58b0b6fe24f8778b6390fa
Ruslan Karabalin <karabalin112@gmail.com>
2022-10-26 15:02:52 +0600

    [+] MyDate, Human, HumanService classes

--- src/main/java/dev/karabalin/sem4/MyDate.java
+++ src/main/java/dev/karabalin/sem4/MyDate.java
@@ -3,13 +3,14 @@ package dev.karabalin.sem4;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Date {
+public class MyDate {
     private int day;
     private String month;
     private int year;
 
-    Map<Integer, String> months = new HashMap<Integer, String>();
-    Map<String, Integer> monthsNum = new HashMap<String, Integer>();
+    private final Map<Integer, String> months = new HashMap<Integer, String>();
+    private final Map<String, Integer> monthsNum = new HashMap<String, Integer>();
+
     {
         months.put(1, "january");
         months.put(2, "february");
@@ -35,18 +36,18 @@ public class Date {
         monthsNum.put("july", 7);
         monthsNum.put("august", 8);
         monthsNum.put("september", 9);
-        monthsNum.put( "october", 10);
-        monthsNum.put( "november", 11);
-        monthsNum.put( "december", 12);
+        monthsNum.put("october", 10);
+        monthsNum.put("november", 11);
+        monthsNum.put("december", 12);
     }
 
-    public Date() {
+    public MyDate() {
         day = 21;
         month = "August";
         year = 7;
     }
 
-    public Date(int day, String month, int year) {
+    public MyDate(int day, String month, int year) {
         if (year > 0 && year <= 2200) {
             this.year = year;
             if (month.equalsIgnoreCase(months.get(1)) ||
@@ -87,7 +88,7 @@ public class Date {
         }
     }
 
-    public Date(int day, int month, int year) {
+    public MyDate(int day, int month, int year) {
         if (year > 0 && year <= 2200) {
             this.year = year;
             if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
@@ -120,16 +121,64 @@ public class Date {
     }
 
     public void setDay(int day) throws IllegalArgumentException {
-        if (day >= 1 && day <= 31) {
-            this.day = day;
-        } else {
-            throw new IllegalArgumentException("Day error");
+        if (this.month.equalsIgnoreCase(months.get(1)) ||
+                this.month.equalsIgnoreCase(months.get(3)) ||
+                this.month.equalsIgnoreCase(months.get(5)) ||
+                this.month.equalsIgnoreCase(months.get(7)) ||
+                this.month.equalsIgnoreCase(months.get(8)) ||
+                this.month.equalsIgnoreCase(months.get(10)) ||
+                this.month.equalsIgnoreCase(months.get(12))) {
+            if (day >= 1 && day <= 31) {
+                this.day = day;
+            } else {
+                throw new IllegalArgumentException("Day error");
+            }
+        } else if (this.month.equalsIgnoreCase(months.get(4)) ||
+                this.month.equalsIgnoreCase(months.get(6)) ||
+                this.month.equalsIgnoreCase(months.get(9)) ||
+                this.month.equalsIgnoreCase(months.get(11))) {
+            if (day >= 1 && day <= 30) {
+                this.day = day;
+            } else {
+                throw new IllegalArgumentException("Day error");
+            }
+        } else if (this.month.equalsIgnoreCase(months.get(2)) && (this.year % 4 == 0)) {
+            if (day >= 1 && day <= 29) {
+                this.day = day;
+            } else {
+                throw new IllegalArgumentException("Day error");
+            }
+        } else if (this.month.equalsIgnoreCase(months.get(2))) {
+            if (day >= 1 && day <= 28) {
+                this.day = day;
+            } else {
+                throw new IllegalArgumentException("Day error");
+            }
         }
     }
 
     public void setMonth(String month) throws IllegalArgumentException {
         if (months.containsValue(month.toLowerCase())) {
-            this.month = month;
+            if (day <= 31 && (month.equalsIgnoreCase(months.get(1)) ||
+                    month.equalsIgnoreCase(months.get(3)) ||
+                    month.equalsIgnoreCase(months.get(5)) ||
+                    month.equalsIgnoreCase(months.get(7)) ||
+                    month.equalsIgnoreCase(months.get(8)) ||
+                    month.equalsIgnoreCase(months.get(10)) ||
+                    month.equalsIgnoreCase(months.get(12)))) {
+                this.month = month;
+            } else if (day <= 30 && (month.equalsIgnoreCase(months.get(4)) ||
+                    month.equalsIgnoreCase(months.get(6)) ||
+                    month.equalsIgnoreCase(months.get(9)) ||
+                    month.equalsIgnoreCase(months.get(11)))) {
+                this.month = month;
+            } else if ((this.year % 4 == 0) && month.equalsIgnoreCase(months.get(2)) && day <= 29) {
+                this.month = month;
+            } else if ((this.year % 4 != 0) && month.equalsIgnoreCase(months.get(2)) && day <= 28) {
+                this.month = month;
+            } else {
+                throw new IllegalArgumentException("Day in month error");
+            }
         } else {
             throw new IllegalArgumentException("Month error");
         }
@@ -144,7 +193,7 @@ public class Date {
     }
 
     public void setYear(int year) throws IllegalArgumentException {
-        if (year >= 0 && year <= 2200) {
+        if (year >= 1970 && year <= 2200) {
             this.year = year;
         } else {
             throw new IllegalArgumentException("Year error");
@@ -163,7 +212,7 @@ public class Date {
         return year;
     }
 
-    public boolean isBeforeNow(Date date) {
+    public boolean isBefore(MyDate date) {
         return (date.day < this.day) && (monthsNum.get(date.month.toLowerCase()) <= monthsNum.get(this.month.toLowerCase())) && (date.year <= this.year);
     }
 }
