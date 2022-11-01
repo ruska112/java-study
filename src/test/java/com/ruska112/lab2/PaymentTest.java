feb465cfca8bdc8bd453a59b260d2eafb4695db4
Ruslan Karabalin <94182636+ruska112@users.noreply.github.com>
2022-11-01 10:31:49 +0600

    Update PaymentTest.java

--- src/test/java/com/ruska112/lab2/PaymentTest.java
+++ src/test/java/com/ruska112/lab2/PaymentTest.java
@@ -1,37 +1,30 @@
 package com.ruska112.lab2;
 
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class PaymentTest {
-    Payment payment1;
-    Payment payment2;
-
-    @Before
-    public void Setup(){
-        payment1 = new Payment("Karabalin Ruslan", 11, 9, 2001, 112112);
-        payment1 = new Payment("Ruslan Karabalin", 11, 9, 2001, 112);
-    }
+    Payment payment1 = new Payment("Karabalin Ruslan", 11, 9, 2001, 112112);
+    Payment payment2 = new Payment("Ruslan Karabalin", 11, 9, 2001, 112);
 
     @Test
     public void equalsTest() {
         assertFalse(payment1.equals(payment2));
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void setFullNameTest() {
         try {
-            String name = new String();
+            String name = "";
             payment1.setFullName(name);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void setSumTest() {
         try {
             payment1.setSum(-1);
