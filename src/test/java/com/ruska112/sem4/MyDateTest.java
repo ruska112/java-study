7b1dfc1bf2e65f66196786971620df59a6018849
Ruslan Karabalin <karabalin112@gmail.com>
2022-11-03 23:18:27 +0600

    [*] edit MyDateTest

--- src/test/java/com/ruska112/sem4/MyDateTest.java
+++ src/test/java/com/ruska112/sem4/MyDateTest.java
@@ -7,12 +7,80 @@ import static org.junit.Assert.*;
 
 public class MyDateTest {
 
-    MyDate date1;
-    MyDate date2;
+    MyDate date1 = new MyDate(2003, "june", 8);
+    MyDate date2 = new MyDate(2003, "april", 4);
 
     @Test
-    public void isBefore() {
-        assertTrue(date2.isBefore(date1));
+    public void isBeforeTest() {
+        assertTrue(date1.isBefore(date2));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorTestYearEL() {
+        MyDate testDate = new MyDate(-1, "january", 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorTestYearEM() {
+        MyDate testDate = new MyDate(3020, "january", 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorTestMonthE() {
+        MyDate testDate = new MyDate(2020, "JabUary", 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorTestMonthEIL() {
+        MyDate testDate = new MyDate(2020, -1, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorTestMonthEIM() {
+        MyDate testDate = new MyDate(2020, 13, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorTestDayEL() {
+        MyDate testDate = new MyDate(2020, "january", -1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorTestDayEM() {
+        MyDate testDate = new MyDate(2020, "january", 69);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setMonthTestS() {
+        MyDate testDate = new MyDate(2020, "january", 1);
+        testDate.setMonth("123");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setMonthTestIM() {
+        MyDate testDate = new MyDate(2020, "january", 1);
+        testDate.setMonth(13);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setDayTestIL() {
+        MyDate testDate = new MyDate(2020, "january", 1);
+        testDate.setDay(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setDayTestIM() {
+        MyDate testDate = new MyDate(2020, "january", 1);
+        testDate.setDay(69);
+    }
+
+    @Test
+    public void setDay2020February29() {
+        MyDate testDate = new MyDate(2020, 2, 29);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setDay2021February29() {
+        MyDate testDate = new MyDate(2021, 2, 29);
+    }
 }
