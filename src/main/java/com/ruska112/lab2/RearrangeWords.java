d7afe95512a76e9b5b0a6a5bb0f147078c409f9d
Ruslan Karabalin <karabalin112@gmail.com>
2022-11-10 21:57:36 +0600

    [*] 5 added more tests, fixed bugs

--- src/main/java/com/ruska112/lab2/RearrangeWords.java
+++ src/main/java/com/ruska112/lab2/RearrangeWords.java
@@ -4,59 +4,67 @@ import java.util.ArrayList;
 
 public class RearrangeWords {
     public static String rearrangeWords(String string) {
-        StringBuffer seq = new StringBuffer(string);
-        ArrayList<String> result = new ArrayList<>();
-        boolean flag = true;
-        int i, j, spaceLen, wordLen;
-        i = 0;
-        j = string.length() - 1;
-        spaceLen = 0;
-        wordLen = 0;
+        if (string == null) {
+            throw new IllegalArgumentException("RearrangeWords rearrangeWords: illegal string argument");
+        } else {
+            if (string.length() != 0) {
+                StringBuffer seq = new StringBuffer(string);
+                ArrayList<String> result = new ArrayList<>();
+                boolean flag = true;
+                int i, j, spaceLen, wordLen;
+                i = 0;
+                j = string.length() - 1;
+                spaceLen = 0;
+                wordLen = 0;
 
-        if (seq.charAt(0) == ' ') {
-            flag = false;
-        }
+                if (seq.charAt(0) == ' ') {
+                    flag = false;
+                }
 
-        while (i < string.length() || j > 0) {
-            if (!flag && i <= string.length()) {
-                if (seq.charAt(i) == ' ') {
-                    spaceLen++;
-                    if (i + 1 != string.length()) {
-                        if (seq.charAt(i + 1) != ' ') {
-                            result.add(seq.substring(i + 1 - spaceLen, i + 1));
-                            flag = true;
-                            spaceLen = 0;
+                while (i < string.length() || j > 0) {
+                    if (!flag && i <= string.length()) {
+                        if (seq.charAt(i) == ' ') {
+                            spaceLen++;
+                            if (i + 1 != string.length()) {
+                                if (seq.charAt(i + 1) != ' ') {
+                                    result.add(seq.substring(i + 1 - spaceLen, i + 1));
+                                    flag = true;
+                                    spaceLen = 0;
+                                }
+                            } else {
+                                result.add(seq.substring(i + 1 - spaceLen, string.length()));
+                                flag = true;
+                            }
                         }
-                    } else {
-                        result.add(seq.substring(i + 1 - spaceLen, string.length()));
-                        flag = true;
+                        i++;
+                    }
+                    if (flag && j >= 0) {
+                        if (seq.charAt(j) != ' ') {
+                            wordLen++;
+                            if (j - 1 != -1) {
+                                if (seq.charAt(j - 1) == ' ') {
+                                    result.add(seq.substring(j, j + wordLen));
+                                    flag = false;
+                                    wordLen = 0;
+                                }
+                            } else {
+                                result.add(seq.substring(j, j + wordLen));
+                                flag = false;
+                            }
+                        }
+                        j--;
                     }
                 }
-                i++;
-            }
-            if (flag && j >= 0) {
-                if (seq.charAt(j) != ' ') {
-                    wordLen++;
-                    if (j - 1 != -1) {
-                        if (seq.charAt(j - 1) == ' ') {
-                            result.add(seq.substring(j, j + wordLen));
-                            flag = false;
-                            wordLen = 0;
-                        }
-                    } else {
-                        result.add(seq.substring(j, j + wordLen));
-                        flag = false;
-                    }
+
+                StringBuffer str = new StringBuffer();
+                for (String s : result) {
+                    str.append(s);
                 }
-                j--;
+
+                return str.toString();
+            } else {
+                return string;
             }
         }
-
-        StringBuffer str = new StringBuffer();
-        for (String s : result) {
-            str.append(s);
-        }
-
-        return str.toString();
     }
 }
