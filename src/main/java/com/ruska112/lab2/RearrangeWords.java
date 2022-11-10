6530c4d78f98ffe7260f2bb8566576a079082a28
Ruslan Karabalin <karabalin112@gmail.com>
2022-11-10 23:13:16 +0600

    [+] 6 added method and tests

--- src/main/java/com/ruska112/lab2/RearrangeWords.java
+++ src/main/java/com/ruska112/lab2/RearrangeWords.java
@@ -3,46 +3,46 @@ package com.ruska112.lab2;
 import java.util.ArrayList;
 
 public class RearrangeWords {
-    public static String rearrangeWords(String string) {
-        if (string == null) {
-            throw new IllegalArgumentException("RearrangeWords rearrangeWords: illegal string argument");
+    public static String rearrangeWords(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("RearrangeWords rearrangeWords: illegal input argument");
         } else {
-            if (string.length() != 0) {
-                StringBuffer seq = new StringBuffer(string);
+            if (input.length() != 0) {
+                StringBuffer string = new StringBuffer(input);
                 StringBuffer result = new StringBuffer();
                 boolean flag = true;
-                int i = 0, j = string.length() - 1, spaceLen = 0, wordLen = 0;
-                if (seq.charAt(0) == ' ') {
+                int i = 0, j = input.length() - 1, spaceLen = 0, wordLen = 0;
+                if (string.charAt(0) == ' ') {
                     flag = false;
                 }
-                while (i < string.length() || j > 0) {
-                    if (!flag && i <= string.length()) {
-                        if (seq.charAt(i) == ' ') {
+                while (i < input.length() || j > 0) {
+                    if (!flag && i <= input.length()) {
+                        if (string.charAt(i) == ' ') {
                             spaceLen++;
-                            if (i + 1 != string.length()) {
-                                if (seq.charAt(i + 1) != ' ') {
-                                    result.append(seq.substring(i + 1 - spaceLen, i + 1));
+                            if (i + 1 != input.length()) {
+                                if (string.charAt(i + 1) != ' ') {
+                                    result.append(string.substring(i + 1 - spaceLen, i + 1));
                                     flag = true;
                                     spaceLen = 0;
                                 }
                             } else {
-                                result.append(seq.substring(i + 1 - spaceLen, string.length()));
+                                result.append(string.substring(i + 1 - spaceLen, input.length()));
                                 flag = true;
                             }
                         }
                         i++;
                     }
                     if (flag && j >= 0) {
-                        if (seq.charAt(j) != ' ') {
+                        if (string.charAt(j) != ' ') {
                             wordLen++;
                             if (j - 1 != -1) {
-                                if (seq.charAt(j - 1) == ' ') {
-                                    result.append(seq.substring(j, j + wordLen));
+                                if (string.charAt(j - 1) == ' ') {
+                                    result.append(string.substring(j, j + wordLen));
                                     flag = false;
                                     wordLen = 0;
                                 }
                             } else {
-                                result.append(seq.substring(j, j + wordLen));
+                                result.append(string.substring(j, j + wordLen));
                                 flag = false;
                             }
                         }
@@ -51,7 +51,7 @@ public class RearrangeWords {
                 }
                 return result.toString();
             } else {
-                return string;
+                return input;
             }
         }
     }
