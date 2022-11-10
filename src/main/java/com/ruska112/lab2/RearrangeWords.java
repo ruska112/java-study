5c5b1302db8a0d762cfca629330439d2c158fb28
Ruslan Karabalin <karabalin112@gmail.com>
2022-11-10 21:30:29 +0600

    [*] 5

--- src/main/java/com/ruska112/lab2/RearrangeWords.java
+++ src/main/java/com/ruska112/lab2/RearrangeWords.java
@@ -1,44 +1,34 @@
 package com.ruska112.lab2;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class RearrangeWords {
     public static String rearrangeWords(String string) {
-        return new String();
-    }
-
-    public static void main(String[] args) {
-        String input = new String(" a  bb   ccc    ");
-        StringBuffer seq = new StringBuffer(input);
+        StringBuffer seq = new StringBuffer(string);
         ArrayList<String> result = new ArrayList<>();
         boolean flag = true;
-        int i, j, spaceLen, wordLen, n;
+        int i, j, spaceLen, wordLen;
         i = 0;
-        j = input.length() - 1;
+        j = string.length() - 1;
         spaceLen = 0;
         wordLen = 0;
-        n = input.length();
 
         if (seq.charAt(0) == ' ') {
             flag = false;
-        } else {
-            flag = true;
         }
 
-        while (i < n || j > 0) {
+        while (i < string.length() || j > 0) {
             if (!flag) {
                 if (seq.charAt(i) == ' ') {
                     spaceLen++;
-                    if (i + 1 != n) {
+                    if (i + 1 != string.length()) {
                         if (seq.charAt(i + 1) != ' ') {
                             result.add(seq.substring(i + 1 - spaceLen, i + 1));
                             flag = true;
                             spaceLen = 0;
                         }
                     } else {
-                        result.add(seq.substring(i + 1 - spaceLen, n));
+                        result.add(seq.substring(i + 1 - spaceLen, string.length()));
                         flag = true;
                     }
                 }
@@ -67,6 +57,6 @@ public class RearrangeWords {
             str.append(s);
         }
 
-        System.out.println(str.toString());
+        return str.toString();
     }
 }
