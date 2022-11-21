e516127f5d535a65108f53f36e71e9fcf1d5e373
Ruslan Karabalin <karabalin112@gmail.com>
2022-11-21 19:51:18 +0600

    [+] ProductBatch

--- src/main/java/com/ruska112/lab3/product/ProductBatch.java
+++ src/main/java/com/ruska112/lab3/product/ProductBatch.java
@@ -1,4 +1,38 @@
 package com.ruska112.lab3.product;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class ProductBatch {
+    private String description;
+    private PackagedProduct[] products;
+
+    public ProductBatch(String description, PackagedProduct... products) {
+        if (description == null) throw new IllegalArgumentException();
+        this.description = description;
+        this.products = products;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public PackagedProduct[] getProducts() {
+        return products;
+    }
+
+    public int hashCode() {
+        return description.hashCode() + Arrays.hashCode(products);
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ProductBatch)) return false;
+        ProductBatch that = (ProductBatch) o;
+        return this.hashCode() == that.hashCode();
+    }
+
+    public String toString() {
+        return String.format("Product Batch: %s \nProducts: %s", description, Arrays.toString(products));
+    }
 }
